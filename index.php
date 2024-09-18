b9b480a29cc13f10b59372abcb63e0fd522bd6c1
JobsnakerID <ciptos213@gmail.com>
2024-09-18 16:58:50 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<style>
+   body{
+      padding-top: 30%;
+      background-color: darkblue;
+      text-align: center;
+      font-family: Times New Roman;
+      font-size: 30px;
+      color: white;
+      
+   }
+</style>
+<body>
+<h1>Welcome to JobsID</h1>
+<img src="JobsID.png" width="300px">
+<form action="candidat/index.php" method="post">
+   <p>Login Sebagai Pencari kerja</p>
+   <p><button type="submit" name="login">Login</button></p>
+</form>
+<form action="recruiter/index.php" method="post">
+   <p>Login Sebagai HR Recruitment</p>
+   <p><button type="submit" name="login">Login</button></p>
+</form>
+</body>
